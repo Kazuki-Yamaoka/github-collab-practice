8ff0fd45c10c5b2805de9afa9215df041547c719
Kazuki Yamaoka <kazuki07y@gmail.com>
2026-08-13 00:58:45 +0900

    add pagenate and validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,16 +9,22 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::pagenate(20);
         return view('users.index', ['users' => $users]);
     }
 
     public function store(Request $request)
     {
+        $validated = $request->$validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique',
+            'password' => 'required|string|min:8',
+        ]);
+
         $user = new User;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
+        $user->name = $validated[name];
+        $user->email = $validated[email];
+        $user->password = $validated(hash::make[password]);
         $user->save();
 
         return redirect('/users');
